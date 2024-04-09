b8ee84f57266817a6de6986bb208fec0ccab3c4a
klominski <147621998+klominski@users.noreply.github.com>
2024-04-09 16:35:55 +0200

    Add files via upload

---
+++ part 3/models/person.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const url = 'mongodb+srv://sebussobczynski:Dfc3x3iOa4FIkorc@phonebook.j508e9k.mongodb.net/?retryWrites=true&w=majority&appName=Phonebook'
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+personSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Person', personSchema)
